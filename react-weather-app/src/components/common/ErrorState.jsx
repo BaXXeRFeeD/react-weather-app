c0a8b4aa87b08b27fcb69fbc1d78551762b7ad13
sorokOut <so_asarkis@vk.com>
2025-12-23 21:12:16 +0300

    Second version

--- react-weather-app/src/components/common/ErrorState.jsx
+++
@@ -1,7 +0,0 @@
-const ErrorState = ({ message }) => (
-    <div className="flex justify-center items-center h-screen text-red-500">
-        {message || 'An error occurred'}
-    </div>
-);
-
-export default ErrorState;
